0759a17998121d3d55654d2517810e1f7dcb8ef4
Rob Pellowski <23526472+RPellowski@users.noreply.github.com>
2018-04-03 09:01:45 -0400

    Add testing of metrics library

--- logmetrics.py
+++ logmetrics.py
@@ -1,50 +1,32 @@
-import logging
+import logging, logging.handlers
 import time
-
-#from appmetrics import metrics
-
-# https://docs.python.org/3/library/logging.handlers.html
-# https://docs.python.org/3/howto/logging-cookbook.html#using-file-rotation
-# http://docs.python-guide.org/en/latest/writing/logging/
-
 import json
-import logging.handlers
 import platform
 
 # globals
 metrics_logger = None
 HOSTNAME = platform.node()
 
-'''
-from resource import getrusage as resource_usage, RUSAGE_SELF
-from time import time as timestamp
+import resource
 
+def unix_time():
+    return (time.time(), resource.getrusage(resource.RUSAGE_SELF))
 
-def unix_time(function, args=tuple(), kwargs={}):
-    Return `real`, `sys` and `user` elapsed time, like UNIX's command `time`
-    You can calculate the amount of used CPU-time used by your
-    function/callable by summing `user` and `sys`. `real` is just like the wall
-    clock.
-    Note that `sys` and `user`'s resolutions are limited by the resolution of
-    the operating system's software clock (check `man 7 time` for more
-    details).
+def unix_time_elapsed(t0, t1):
+    return {'unix_time_elapsed' : {
+               'real': t1[0] - t0[0],
+               'sys': t1[1].ru_stime - t0[1].ru_stime,
+               'user': t1[1].ru_utime - t0[1].ru_utime
+               }
+           }
 
-    start_time, start_resources = timestamp(), resource_usage(RUSAGE_SELF)
-    function(*args, **kwargs)
-    end_resources, end_time = resource_usage(RUSAGE_SELF), timestamp()
-
-    return {'real': end_time - start_time,
-            'sys': end_resources.ru_stime - start_resources.ru_stime,
-            'user': end_resources.ru_utime - start_resources.ru_utime}
-
-'''
-DATEFMT = '%Y-%m-%dT%H:%M:%SZ'
+# ISO 8601 same as '%Y-%m-%dT%H:%M:%SZ'
+DATEFMT = '%FT%TZ'
 FMT = '%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(message)s'
 
 class json_formatter(logging.Formatter):
     def __init__(self, task_name=None):
         self.task_name = task_name
-
         super(json_formatter, self).__init__()
 
     def format(self, record):
@@ -53,7 +35,8 @@ class json_formatter(logging.Formatter):
                 'timestring': time.strftime(DATEFMT, time.gmtime(record.created)),
                 'filename': record.filename,
                 'level': record.levelname,
-                'function': record.funcName
+                'function': record.funcName,
+                'hostname': HOSTNAME
                 }
         #'asctime': time.asctime(time.gmtime(record.created)),
         return json.dumps(data)
@@ -69,6 +52,7 @@ def initLogger(logger_level = logging.DEBUG,
     metrics_logger = logging.getLogger(__name__)
     metrics_logger.setLevel(logger_level)
 
+    # console handler
     if console_logging != 'none':
         # create handler for console output
         console_handler = logging.StreamHandler()
@@ -81,6 +65,7 @@ def initLogger(logger_level = logging.DEBUG,
         console_handler.setLevel(console_level)
         metrics_logger.addHandler(console_handler)
 
+    # file handler
     if file_logging != 'none' and not (file_out == '' or file_out is None):
         # create handler for file output
         file_handler = logging.FileHandler(filename=file_out)
--- logtester.py
+++ logtester.py
@@ -1,27 +1,73 @@
 import logging, logmetrics
 
-def hello():
-    return "Hello World!\n"
-
 def log_debug_message():
     logger.debug('debug message')
-    return "Logged debug message.\n"
 
 def log_info_message():
     logger.info('info message')
-    return "Logged info message.\n"
 
 def log_warning_message():
     logger.warning('warning message')
-    return "Logged warning message\n"
+
+from appmetrics import metrics
+import time
+import random
+
+def test_histogram():
+    @metrics.with_histogram("test1")
+    def my_worker():
+        time.sleep(random.random())
+
+    # test timing histogram
+    my_worker()
+    my_worker()
+    my_worker()
+    logger.debug(metrics.get("test1"))
+
+    # test data histogram
+    histogram = metrics.new_histogram("test2")
+    histogram.notify(1.0)
+    histogram.notify(2.0)
+    histogram.notify(3.0)
+    logger.debug(histogram.get())
+
+def test_counter():
+    counter = metrics.new_counter("test")
+    counter.notify(10)
+    logger.debug(counter.get())
+
+def test_gauge():
+    gauge = metrics.new_gauge("gauge_test")
+    gauge.notify("version 1.0")
+    logger.debug(gauge.get())
+
+def test_meter():
+    meter = metrics.new_meter("meter_test")
+    meter.notify(1)
+    meter.notify(1)
+    meter.notify(3)
+    logger.debug(meter.get())
+
 
 if __name__ == '__main__':
-    hello()
     global logger
-    logger = logmetrics.initLogger()
+    logger = logmetrics.initLogger() #console_logging='json', file_logging='none')
     log_debug_message()
     log_info_message()
     log_warning_message()
-# test
+
+    t0 = logmetrics.unix_time()
+    for i in range(100000):
+        pass
+    t1 = logmetrics.unix_time()
+    logger.debug(logmetrics.unix_time_elapsed(t0, t1))
+
+    test_histogram()
+    test_gauge()
+    test_counter()
+    test_meter()
+
+# testing strategy for automation
 # logging.disable() default is CRITICAL for 3.7
 # logging.disable(logging.NOTSET)
+
