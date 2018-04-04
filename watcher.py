18d730dd679700b6ea755c7d868f3def421328da
Rob Pellowski <23526472+RPellowski@users.noreply.github.com>
2018-04-04 19:21:02 -0400

    Deliver file system activity logger

--- watcher.py
+++ watcher.py
@@ -1,77 +1,52 @@
 import sys
 import time
 import logging
+import logmetrics
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler, FileSystemEventHandler
+from watchdog.events import FileSystemEventHandler
 from appmetrics import metrics
 
-class Event():
-    def __init__(self, event_type, src_path, is_directory):
-        self.event_type = event_type
-        self.src_path = src_path
-        self.is_directory = is_directory
+FS_BUCKETS = ['code', 'doc', 'other']
+EXT = {
+      '.py'  : 'code',
+      '.md'  : 'doc',
+      '.txt' : 'doc',
+      '.log' : 'skip',
+      '.pyc' : 'skip',
+      }
 
-#class Handler(FileSystemEventHandler):
-def test_events():
-    ext = {
-          '.py'  : 'code',
-          '.md'  : 'doc',
-          '.txt' : 'doc'
-          }
+class Handler(FileSystemEventHandler):
     def on_any_event(self, event):
-        e = [v for k,v in ext.items() if event.src_path.endswith(k)]
-        if event.is_directory :
+        e = [v for k,v in EXT.items() if event.src_path.endswith(k)]
+        if event.is_directory or len(e) == 0:
             metrics.metric('other').notify(1)
-        elif len(e) > 0:
-            print(e[0])
+        elif not (e[0] is 'skip'):
             metrics.metric(e[0]).notify(1)
-        else:
-            metrics.metric('other').notify(1)
-    for f in ['my.py', 'my.txt', '4914', '.']:
-        for t in ['modified', 'deleted', 'moved', 'created']:
-            event = Event(t, f, True if f is '.' else False)
-            on_any_event(None, event)
-    #logging.info({"event":[event.event_type, event.is_directory, event.src_path]})
+        #logger.info({"event" : [event.event_type, event.is_directory, event.src_path]})
 
-if __name__ == "__xmain__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
+
+if __name__ == "__main__":
+
+    global logger
+    logger = logmetrics.initLogger(console_logging='json', file_out = 'activity.log')
+
+    global counters
+    counters = [metrics.new_counter(f) for f in FS_BUCKETS]
+
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    #event_handler = LoggingEventHandler()
     event_handler = Handler()
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
 
-    #counter
     observer.start()
     try:
         while True:
-            time.sleep(1)
-            for f in ['code', 'doc', 'other']:
-            #logging.info({"event":[event.event_type, event.is_directory, event.src_path]})
+            time.sleep(60)
+            for f in FS_BUCKETS:
+                if metrics.metric(f).raw_data() > 0:
+                    logger.info({f: metrics.get(f)})
+                    metrics.metric(f).notify(0 - metrics.metric(f).raw_data())
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
 
-#every minute, if bucket > 0, log all event counts
-# bucket based on filetype: documentation/code/other
-# any change to that bucket increments one of the counters: created/deleted/modified/moved
-
-else:
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    global counters
-    counters = [metrics.new_counter(f) for f in ['code', 'doc', 'other']]
-    #counters = [metrics.new_counter(" ".join((f, t)))
-    #    for f in ['code', 'doc', 'other']
-    #        for t in ['modified', 'deleted', 'moved', 'created']
-    #    ]
-    test_events()
-    for f in ['code', 'doc', 'other']:
-        print(f, metrics.get(f))
-    for f in ['code', 'doc', 'other']:
-        metrics.metric(f).notify(0 - metrics.metric(f).raw_data())
-    for f in ['code', 'doc', 'other']:
-        print(f, metrics.get(f))
